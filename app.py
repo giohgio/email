06f4902ac68e0abb6131e51de57be3238ee73d7e
giohgio <giooh.giooo15@gmail.com>
2025-10-06 13:14:10 -0300

    Update232142342353465yhgfbgsdh app.py

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def webhook_listener():
-    # Obtenha os dados enviados pelo Mercado Pago
+    if request.method == 'GET':
+        return jsonify({'status': 'ok'}), 200
+
+    # POST
     data = request.json
     print("Webhook recebido:", data)
 
@@ -13,17 +16,13 @@ def webhook_listener():
     # Para mais informações, consulte a documentação do Mercado Pago.
 
     # Processar os dados da notificação
-    # Exemplo: Verificar o status do pagamento e atualizar o status no seu banco de dados
     if 'type' in data and data['type'] == 'payment':
         if 'data' in data and 'id' in data['data']:
             payment_id = data['data']['id']
-            # Faça uma requisição à API do Mercado Pago para obter os detalhes do pagamento e confirmar o status
             print(f"Detalhes do pagamento: {payment_id}")
             # Adicione sua lógica aqui para processar o pagamento
 
-    # Responda com um status 200 OK para confirmar o recebimento
     return jsonify({'status': 'success'}), 200
 
 if __name__ == '__main__':
-    # Para ambientes de produção, use um servidor WSGI como Gunicorn
-    app.run(port=5000)
+    app.run(host="0.0.0.0", port=5000)
